01244d2c29521c72172d8ae6bacef21359eb53bc
Nidhal <nidhaldjerbi@gmail.com>
2018-03-26 20:13:09 +0200

    intiation gestion profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
